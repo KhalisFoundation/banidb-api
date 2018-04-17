25125e1072046f00d809ce3fbc372c75aa290d83
Navdeep Singh <me@navdeepsingh.ca>
2018-04-17 04:43:15 -0700

    Use /v2 base path

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,41 +1,44 @@
+const express = require('express');
 const os = require('os');
 const pjson = require('../../package.json');
 const shabads = require('../controllers/shabads');
 const banis = require('../controllers/banis');
 const rehats = require('../controllers/rehats');
 
-module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.json({
-      name: 'BaniDB API',
-      docs: 'See https://www.banidb.com for more information and documentation.',
-      version: pjson.version,
-      endpoint: os.hostname().substr(0, 3),
-    });
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'BaniDB API',
+    docs: 'See https://www.banidb.com for more information and documentation.',
+    version: pjson.version,
+    endpoint: os.hostname().substr(0, 3),
   });
+});
 
-  // Shabad Routes
-  app.get('/search/:query', shabads.search);
+// Shabad Routes
+app.get('/search/:query', shabads.search);
 
-  app.get('/shabads/:ShabadID', shabads.shabads);
+app.get('/shabads/:ShabadID', shabads.shabads);
 
-  app.get('/angs/:PageNo/:SourceID?', shabads.angs);
+app.get('/angs/:PageNo/:SourceID?', shabads.angs);
 
-  app.get('/hukamnamas/:year?/:month?/:day?', shabads.hukamnamas);
+app.get('/hukamnamas/:year?/:month?/:day?', shabads.hukamnamas);
 
-  app.get('/random/:SourceID?', shabads.random);
+app.get('/random/:SourceID?', shabads.random);
 
-  // Bani Routes
-  app.get('/banis', banis.all);
+// Bani Routes
+app.get('/banis', banis.all);
 
-  app.get('/banis/:BaniID', banis.bani);
+app.get('/banis/:BaniID', banis.bani);
 
-  // Rehat Routes
-  app.get('/rehats', rehats.all);
+// Rehat Routes
+app.get('/rehats', rehats.all);
 
-  app.get('/rehats/:RehatID', rehats.chapterList);
+app.get('/rehats/:RehatID', rehats.chapterList);
 
-  app.get('/rehats/:RehatID/chapters/:ChapterID?', rehats.chapters);
+app.get('/rehats/:RehatID/chapters/:ChapterID?', rehats.chapters);
 
-  app.get('/rehats/search/:string', rehats.search);
-};
+app.get('/rehats/search/:string', rehats.search);
+
+module.exports = app;
--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const routes = require('./api/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/v2', routes);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-routes(app);
 
 app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
