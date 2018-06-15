46f5550f807331834cc624fbe5d185369c3f8099
Divjot Singh <bogas04@gmail.com>
2018-06-15 19:03:16 +0530

    Set CORS before APIs

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,44 +1,44 @@
-const express = require('express');
 const os = require('os');
+const { Router } = require('express');
 const pjson = require('../../package.json');
 const shabads = require('../controllers/shabads');
 const banis = require('../controllers/banis');
 const rehats = require('../controllers/rehats');
 
-const app = express();
+const route = Router();
 
-app.get('/', (req, res) => {
+route.get('/', (req, res) => {
   res.json({
     name: 'BaniDB API',
     docs: 'See https://www.banidb.com for more information and documentation.',
     version: pjson.version,
-    endpoint: os.hostname().substr(0, 3),
+    endpoint: os.hostname().substr(0, 3)
   });
 });
 
 // Shabad Routes
-app.get('/search/:query', shabads.search);
+route.get('/search/:query', shabads.search);
 
-app.get('/shabads/:ShabadID', shabads.shabads);
+route.get('/shabads/:ShabadID', shabads.shabads);
 
-app.get('/angs/:PageNo/:SourceID?', shabads.angs);
+route.get('/angs/:PageNo/:SourceID?', shabads.angs);
 
-app.get('/hukamnamas/:year?/:month?/:day?', shabads.hukamnamas);
+route.get('/hukamnamas/:year?/:month?/:day?', shabads.hukamnamas);
 
-app.get('/random/:SourceID?', shabads.random);
+route.get('/random/:SourceID?', shabads.random);
 
 // Bani Routes
-app.get('/banis', banis.all);
+route.get('/banis', banis.all);
 
-app.get('/banis/:BaniID', banis.bani);
+route.get('/banis/:BaniID', banis.bani);
 
 // Rehat Routes
-app.get('/rehats', rehats.all);
+route.get('/rehats', rehats.all);
 
-app.get('/rehats/:RehatID', rehats.chapterList);
+route.get('/rehats/:RehatID', rehats.chapterList);
 
-app.get('/rehats/:RehatID/chapters/:ChapterID?', rehats.chapters);
+route.get('/rehats/:RehatID/chapters/:ChapterID?', rehats.chapters);
 
-app.get('/rehats/search/:string', rehats.search);
+route.get('/rehats/search/:string', rehats.search);
 
-module.exports = app;
+module.exports = route;
--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const routes = require('./api/routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/v2', routes);
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/v2', routes);
 
 app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
