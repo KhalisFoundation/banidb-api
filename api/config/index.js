a048d5894dfd366f9df1f5ee5ad763ae524a280e
Divjot Singh <bogas04@gmail.com>
2018-06-15 18:19:33 +0530

    Accept DB_* env vars for credentials

--- api/config/index.js
+++ api/config/index.js
@@ -1,9 +1,8 @@
 module.exports = {
   mysql: {
     host: '127.0.0.1',
-    // Make sure we use correct user/password in prod.
-    user: 'root',
-    password: 'root',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
     database: 'khajana_khajana',
     dateStrings: true
   }
