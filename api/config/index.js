23fb8159cf9f26706cc88cd3a1033dbcfd2c32f5
Divjot Singh <bogas04@gmail.com>
2018-06-15 18:15:20 +0530

    Fix query for Banis

--- api/config/index.js
+++ api/config/index.js
@@ -1,9 +1,10 @@
 module.exports = {
   mysql: {
     host: '127.0.0.1',
+    // Make sure we use correct user/password in prod.
     user: 'root',
     password: 'root',
     database: 'khajana_khajana',
-    dateStrings: true,
-  },
+    dateStrings: true
+  }
 };
--- api/controllers/banis.js
+++ api/controllers/banis.js
@@ -1,26 +1,44 @@
 const { createPool } = require('mysql');
 const config = require('../config');
-const {
-  prepVerse,
-  getSource,
-  getRaag,
-  getWriter,
-} = require('./getJSON');
+const { prepVerse, getSource, getRaag, getWriter } = require('./getJSON');
 
 const pool = createPool(config.mysql);
 const query = pool.query.bind(pool);
 
-const allColumns = `b.Gurmukhi AS NameGurmukhi,
-  b.GurmukhiUni AS NameGurmukhiUni, b.Transliteration AS NameTransliteration,
-  v.Gurmukhi, v.GurmukhiUni, v.English, v.Punjabi,
-  v.PunjabiUni, v.Spanish, v.PageNo AS PageNo, v.LineNo,
-  v.SourceID, v.Bisram, v.igurbani_bisram1, v.igurbani_bisram2,
-  v.Transliteration, v.WriterID, w.WriterEnglish,
-  w.WriterGurmukhi, w.WriterUnicode, v.RaagID, r.RaagGurmukhi,
-  r.RaagUnicode, r.RaagEnglish, r.RaagWithPage, src.SourceGurmukhi,
-  src.SourceUnicode, src.SourceEnglish, v.header, v.MangalPosition,
-  v.existsStandard, v.existsTaksal, v.existsBuddhaDal, v.Paragraph,
-  v.Updated
+const allColumns = `
+b.Gurmukhi AS NameGurmukhi,
+b.GurmukhiUni AS NameGurmukhiUni,
+b.Transliteration AS NameTransliteration,
+v.Gurmukhi,
+v.GurmukhiUni,
+v.English,
+v.Punjabi,
+v.PunjabiUni,
+v.Spanish,
+v.PageNo AS PageNo,
+v.LineNo,
+v.SourceID,
+v.GurmukhiBisram,
+v.Transliteration,
+v.WriterID,
+w.WriterEnglish,
+w.WriterGurmukhi,
+w.WriterUnicode,
+v.RaagID,
+r.RaagGurmukhi,
+r.RaagUnicode,
+r.RaagEnglish,
+r.RaagWithPage,
+src.SourceGurmukhi,
+src.SourceUnicode,
+src.SourceEnglish,
+v.header,
+v.MangalPosition,
+v.existsStandard,
+v.existsTaksal,
+v.existsBuddhaDal,
+v.ParagraphShort,
+v.Updated
 FROM mv_Banis_Shabad v
 LEFT JOIN Banis b ON b.ID=v.Bani
 LEFT JOIN Writer w USING(WriterID)
@@ -28,41 +46,42 @@ LEFT JOIN Raag r USING(RaagID)
 LEFT JOIN Source src USING(SourceID)`;
 
 exports.all = (req, res) => {
-  const q = 'SELECT ID, Token as token, Gurmukhi as gurmukhi, GurmukhiUni as gurmukhiUni, Transliteration as transliteration, Updated as updated FROM Banis WHERE ID < 1000 ORDER BY ID ASC';
-  query(
-    q,
-    [],
-    (err, rows) => {
-      res.json(rows);
+  const q =
+    'SELECT ID, Token as token, Gurmukhi as gurmukhi, GurmukhiUni as gurmukhiUni, Transliteration as transliteration, Updated as updated FROM Banis WHERE ID < 1000 ORDER BY ID ASC';
+  query(q, [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: true, data: err });
+      return;
     }
-  );
+    res.json(rows);
+  });
 };
 
 exports.bani = (req, res) => {
   const BaniID = parseInt(req.params.BaniID, 10);
   const q = `SELECT ${allColumns} WHERE v.Bani = ? ORDER BY Seq ASC`;
-  query(
-    q,
-    [BaniID],
-    (err, rows) => {
-      const baniInfo = {
-        baniID: BaniID,
-        gurmukhi: rows[0].NameGurmukhi,
-        unicode: rows[0].NameGurmukhiUni,
-        english: rows[0].NameTransliteration,
-        source: getSource(rows[0]),
-        raag: getRaag(rows[0]),
-        writer: getWriter(rows[0]),
-      };
-
-      const verses = rows.map(prepBaniVerse);
-
-      res.json({
-        baniInfo,
-        verses,
-      });
+  query(q, [BaniID], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: true, data: err });
+      return;
     }
-  );
+    const baniInfo = {
+      baniID: BaniID,
+      gurmukhi: rows[0].NameGurmukhi,
+      unicode: rows[0].NameGurmukhiUni,
+      english: rows[0].NameTransliteration,
+      source: getSource(rows[0]),
+      raag: getRaag(rows[0]),
+      writer: getWriter(rows[0])
+    };
+
+    const verses = rows.map(prepBaniVerse);
+
+    res.json({
+      baniInfo,
+      verses
+    });
+  });
 };
 
 function prepBaniVerse(row) {
@@ -74,6 +93,6 @@ function prepBaniVerse(row) {
     existsStandard: row.existsStandard,
     existsTaksal: row.existsTaksal,
     existsBuddhaDal: row.existsBuddhaDal,
-    paragraph: row.Paragraph,
+    paragraph: row.Paragraph
   });
 }
