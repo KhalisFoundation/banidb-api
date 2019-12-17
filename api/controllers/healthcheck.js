d065408aaf2e6b089322d4aac5368be875c3a248
Manjot Singh <manjot@manjotsingh.net>
2019-12-17 08:40:32 -0800

    fix issues from review

--- api/controllers/healthcheck.js
+++ api/controllers/healthcheck.js
@@ -8,12 +8,14 @@ exports.db = async (req, res) => {
     const pool = createPool({ ...config.mysql, connectTimeout: 3000 });
     conn = await pool.getConnection();
     const q = `SELECT 1 as ok FROM Verse WHERE ID=1 LIMIT 1`;
-    const rows = await conn.query(q, []);
+    const rows = await conn.query(q);
     rows[0].ok = rows[0].ok === 1;
     res.json(rows[0]);
   } catch (err) {
     lib.error(err, res, 500, false);
   } finally {
-    if (conn) conn.end();
+    if (conn) {
+      conn.end();
+    }
   }
 };
