343e9ee003ca574568d6d41d88157e788230c5f5
Taranjit Singh <tsingh777@gmail.com>
2020-02-04 18:41:38 -0800

    Add swagger and api-docs endpoint

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cacheControl = require('express-cache-controller');
 const { createPoolCluster } = require('mariadb');
+const swaggerUi = require('swagger-ui-express');
+
 const config = require('./api/config');
 const routes = require('./api/routes');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? '3001' : '3000';
@@ -20,6 +23,7 @@ app.use(cacheControl({ maxAge: 21600 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/v2', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.cacheControl = { noCache: true };
