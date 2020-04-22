a2dc201990362f100d64c4368eb6e00180d7fe2d
Manjot Singh <manjot@manjotsingh.net>
2020-04-22 13:40:29 -0700

    Get json ceremonies compat (#151)
    * Updated getJSON library to handle varying inputs, including verse ranges for ceremonies
    
    dependency: sttm-api
    
    * Added SQL update
    
    * Fix test issues
    
    * increment version
    
    * eslint sucks
    
    * remove console logging!
    
    * bump version
    
    * separate visraams to method
    
    * fix review issues

--- api/lib/searchOperators.js
+++ api/lib/searchOperators.js
@@ -435,13 +435,7 @@ module.exports = {
     const qOutOr = qOR.join(' OR ');
     qAND.push(`(${qOutOr})`);
     const qOut = qAND.join(' AND ');
-    console.log({
-      q: qOut,
-      parameters,
-      least,
-      most,
-      totalPages: ltgt ? null : parameters.length,
-    });
+
     return {
       q: qOut,
       parameters,
