174d70f9ffe6507e58f470318d60b820399d616e
Taranjit Singh <tsingh777@gmail.com>
2020-05-02 22:16:00 -0700

    update start strict

--- bin/start-docker.sh
+++ bin/start-docker.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-docker run -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=khajana_dev_khajana -d -p 3306:3306 --name banidb-api khalisfoundation/banidb-dev:latest
+
+container_name=banidb-api
+is_running=`docker inspect -f '{{.State.Running}}' $container_name`
+
+if [ -z "$is_running" ]
+then
+     echo "Starting docker container"
+     docker run -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=khajana_dev_khajana -d -p 3306:3306 --name $container_name khalisfoundation/banidb-dev:latest
+else
+      echo "Docker contatiner is running"
+fi
