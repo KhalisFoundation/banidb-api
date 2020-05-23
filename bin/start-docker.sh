5f48cc572cc4b575e2affb103cdfc32af9799263
Taranjit Singh <tsingh777@gmail.com>
2020-05-23 14:38:13 -0700

    add await for container to be ready and start script DB port

--- bin/start-docker.sh
+++ bin/start-docker.sh
@@ -3,10 +3,30 @@
 container_name=banidb-api
 is_running=`docker inspect -f '{{.State.Running}}' $container_name`
 
+if [ -z "${DB_PORT}" ]; then 
+    dbPort=3306
+else 
+    dbPort=${DB_PORT}
+fi
+
 if [ -z "$is_running" ]
 then
      echo "Starting docker container"
-     docker run -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=khajana_dev_khajana -d -p 3306:3306 --name $container_name khalisfoundation/banidb-dev:latest
+     docker run -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=khajana_dev_khajana -d -p $dbPort:3306 --name $container_name khalisfoundation/banidb-dev:latest
+     attempt=0
+      while [ $attempt -le 59 ]; do
+      attempt=$(( $attempt + 1 ))
+      echo "Waiting for container to be up (attempt: $attempt)..."
+      result=$(docker exec banidb-api mysqladmin ping -u root -proot 2> /dev/null || echo 'fail' ) 
+      if grep -q 'mysqld is alive' <<< $result ; then
+            echo "DB container is up"
+            echo "Waiting so connections stabalize"
+            break
+      fi
+      sleep 10
+      done
+      sleep 50
+      echo "~~~~Ready~~~~~~"
 else
       echo "Docker container is running"
 fi
