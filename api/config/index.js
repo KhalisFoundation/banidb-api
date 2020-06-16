5a7b1a3144d5f393031345c8fab85a5f6e932078
Manjot Singh <manjot@manjotsingh.net>
2020-06-16 16:08:34 -0700

    try/catch

--- api/config/index.js
+++ api/config/index.js
@@ -1,11 +1,30 @@
+const metadata = {
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.NODE_ENV === 'development' ? 'khajana_dev_khajana' : 'khajana_khajana',
+  dateStrings: true,
+  connectionLimit: process.env.DB_POOL_SIZE,
+};
+
 module.exports = {
-  mysql: {
+  mysql0: {
     host: '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.NODE_ENV === 'development' ? 'khajana_dev_khajana' : 'khajana_khajana',
-    dateStrings: true,
-    connectionLimit: process.env.DB_POOL_SIZE,
+    port: 1, //process.env.DB_PORT || 3306,
+    ...metadata,
+  },
+  mysql2: {
+    host: 'db1.khalis.net',
+    port: 3306,
+    ...metadata,
+  },
+  mysql2: {
+    host: 'db2.khalis.net',
+    port: 3306,
+    ...metadata,
+  },
+  mysql2: {
+    host: 'db3.khalis.net',
+    port: 3306,
+    ...metadata,
   },
 };
--- app.js
+++ app.js
@@ -9,8 +9,27 @@ const routes = require('./api/routes');
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? '3001' : '3000';
 
-app.locals.pool = createPool(config.mysql);
+//database
+try {
+  app.locals.pool = createPool(config.mysql0);
+} catch (err) {
+  try {
+    app.locals.pool = createPool(config.mysql1);
+  } catch (err) {
+    try {
+      app.locals.pool = createPool(config.mysql2);
+    } catch (err) {
+      try {
+        app.locals.pool = createPool(config.mysql3);
+      } catch (err) {
+        console.log('Could not connect to any database!');
+        console.log(err);
+      }
+    }
+  }
+}
 
+// app
 app.use(cors());
 app.use(cacheControl({ maxAge: 21600 }));
 app.use(bodyParser.urlencoded({ extended: true }));
