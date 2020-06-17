8e635d3e0c64ce26542845ac754f01dc41491d4c
Manjot Singh <manjot@manjotsingh.net>
2020-06-17 09:00:21 -0700

    More transl srcs (#182)
    * fix issue with blank visraam #181
    add manmohan singh to punjabi
    fix some docker script problems
    
    * Automatically serve any available punjabi translations
    
    * remove lodash
    
    * resolving issues from sttm-api repo
    
    * fix for ceremonies
    
    * remove lodash
    
    * Delete package-lock.json

--- api/config/index.js
+++ api/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   mysql: {
-    host: '127.0.0.1',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
