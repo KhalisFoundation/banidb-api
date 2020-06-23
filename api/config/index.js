e844702654832aaa0be40263f5bccfa9134e0753
Manjot Singh <manjot@manjotsingh.net>
2020-06-23 09:24:29 -0700

    Failover db (#186)
    * Adds failover to database connection #179

--- api/config/index.js
+++ api/config/index.js
@@ -1,11 +1,35 @@
+const metadata = {
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.NODE_ENV === 'development' ? 'khajana_dev_khajana' : 'khajana_khajana',
+  dateStrings: true,
+  acquireTimeout: 5000,
+  connectionLimit: process.env.DB_POOL_SIZE,
+};
+const standbyMetadata = {
+  port: 3306,
+  minimumIdle: 0,
+};
+
 module.exports = {
-  mysql: {
+  mysql0: {
+    ...metadata,
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.NODE_ENV === 'development' ? 'khajana_dev_khajana' : 'khajana_khajana',
-    dateStrings: true,
-    connectionLimit: process.env.DB_POOL_SIZE,
+  },
+  mysql1: {
+    ...metadata,
+    ...standbyMetadata,
+    host: 'db1.khalis.net',
+  },
+  mysql2: {
+    ...metadata,
+    ...standbyMetadata,
+    host: 'db2.khalis.net',
+  },
+  mysql3: {
+    ...metadata,
+    ...standbyMetadata,
+    host: 'db3.khalis.net',
   },
 };
--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cacheControl = require('express-cache-controller');
-const { createPool } = require('mariadb');
+const { createPoolCluster } = require('mariadb');
 const config = require('./api/config');
 const routes = require('./api/routes');
 
 const app = express();
 const port = process.env.NODE_ENV === 'development' ? '3001' : '3000';
 
-app.locals.pool = createPool(config.mysql);
+// database
+app.locals.pool = createPoolCluster();
+app.locals.pool.add('local', config.mysql0);
+app.locals.pool.add('db1', config.mysql1);
+app.locals.pool.add('db2', config.mysql2);
+app.locals.pool.add('db3', config.mysql3);
 
+// app
 app.use(cors());
 app.use(cacheControl({ maxAge: 21600 }));
 app.use(bodyParser.urlencoded({ extended: true }));
