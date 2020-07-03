dd8439342e88f434dc453f4020c0869e24eea6cb
Manjot Singh <manjot@manjotsingh.net>
2020-07-02 19:28:25 -0700

    Dbname to process (#189)
    * Move db configs to process env
    
    * increment version
    
    * Review fixes - change to array and add examples
    
    * remove extra truthiness
    
    * add package-lock.json back

--- api/config/index.js
+++ api/config/index.js
@@ -1,3 +1,36 @@
+/**
+ * Expects process-dev.json or process.json (or run local)
+ *
+ * @example
+ *
+ * {
+ *    "name" : "banidb",
+ *    "script" : "app.js",
+ *    "env": {
+ *        "NODE_ENV": "development|production",
+ *        "DB_USER": "",
+ *        "DB_PASSWORD": "",
+ *        "DB_POOL_SIZE": 5,
+ *        "DB_NODES": [
+ *          {
+ *            "host": "localhost",
+ *            "port": 3306,
+ *            "isPrimary": true,
+ *          },
+ *          {
+ *            "host": "dba.contoso.net",
+ *          },
+ *          {
+ *            "host": "dbb.contoso.net",
+ *          },
+ *        ],
+ *    },
+ *    "error_file": "err.log",
+ *    "out_file": "out.log",
+ *    "log_date_format": "YYYY-MM-DD HH:mm:ss Z"
+ * }
+ */
+
 const metadata = {
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
@@ -7,29 +40,36 @@ const metadata = {
   connectionLimit: process.env.DB_POOL_SIZE,
 };
 const standbyMetadata = {
-  port: 3306,
   minimumIdle: 0,
 };
 
-module.exports = {
-  mysql0: {
+// everything below is to pull from process.json
+const configArry = [];
+
+// if npm run local, then just define what cli gives, otherwise use process.json
+if (!!process.env.DB_NODES) {
+  const dbs = JSON.parse(process.env.DB_NODES);
+  dbs.forEach(db => {
+    const thisObj = {
+      ...metadata,
+      host: db.host,
+      port: db.port || 3306,
+    };
+    if (!!db.isPrimary) {
+      configArry.push(thisObj);
+    } else {
+      configArry.push({
+        ...thisObj,
+        ...standbyMetadata,
+      });
+    }
+  });
+} else {
+  configArry.push({
     ...metadata,
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-  },
-  mysql1: {
-    ...metadata,
-    ...standbyMetadata,
-    host: 'db1.khalis.net',
-  },
-  mysql2: {
-    ...metadata,
-    ...standbyMetadata,
-    host: 'db2.khalis.net',
-  },
-  mysql3: {
-    ...metadata,
-    ...standbyMetadata,
-    host: 'db3.khalis.net',
-  },
-};
+  });
+}
+
+module.exports = configArry;
