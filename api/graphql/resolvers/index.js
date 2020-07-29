1fe78b2399d5862a1d688ed450b69721e015f64e
tsingh777 <tsingh777@gmail.com>
2020-07-28 19:14:27 -0700

    Add graphql query for ang, multiAng, and multiShabad. Fix single Shabad bug (#204)
    * Add graphql for ang, multiAng, and multi-shabad. Fix single Shabad bug
    
    * address code review

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -1,8 +1,9 @@
 const { mergeResolvers } = require('@graphql-tools/merge');
 const shabadResolver = require('./shabadResolver');
 const baniResolver = require('./baniResolver');
+const angResolver = require('./angResolver');
 
-const resolvers = [shabadResolver, baniResolver];
+const resolvers = [shabadResolver, baniResolver, angResolver];
 
 const mergedResolvers = mergeResolvers(resolvers);
 
